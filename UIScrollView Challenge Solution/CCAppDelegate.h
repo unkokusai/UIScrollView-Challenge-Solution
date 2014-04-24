8679afbcf96ae50c723782aed20e786c5fdeaae7
Yasuyuki Pham <ypham@Yasuyukis-Mac-mini.local>
2014-04-23 19:30:44 -0700

    Initial Commit

---
+++ UIScrollView Challenge Solution/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  UIScrollView Challenge Solution
+//
+//  Created by Yasuyuki Pham on 4/23/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
